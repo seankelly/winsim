8433ebd749b66dd26dc49953a0c3df11a57a0762
Sean Kelly <code@katron.org>
2018-02-01 08:26:04 -0500

    Load teams in the table
    Need to shorten the win percentages. Displaying them with only three
    digits is good enough. The full percentage can be saved as a data
    element.

--- winsim.ts
+++ winsim.ts
@@ -20,11 +20,43 @@ function init() {
         });
 }
 
+function load_season(year) {
+    let season_file = 'mlb-' + year + '.json';
+    jQuery.getJSON(season_file)
+        .done(function(data) {
+            let teams = document.createDocumentFragment();
+            // TODO: Clear body.
+            let body = document.createElement("tbody");
+            teams.appendChild(body);
+            for (let team of data.teams) {
+                let row = document.createElement("tr");
+                let name = document.createElement("td");
+                name.innerText = team.name;
+                let win = document.createElement("td");
+                win.innerText = team.win_percentage;
+                let pythagenpat = document.createElement("td");
+                pythagenpat.innerText = team.pythagenpat_percentage;
+                let baseruns = document.createElement("td");
+                baseruns.innerText = team.baseruns_percentage;
+
+                row.appendChild(name);
+                row.appendChild(win)
+                row.appendChild(pythagenpat)
+                row.appendChild(baseruns)
+                body.appendChild(row);
+            }
+            teams.appendChild(body);
+            document.getElementById("teams").appendChild(teams);
+        });
+}
+
 function pick_season(ev) {
     let year = ev.target.value;
     if (!year) {
         return;
     }
+
+    load_season(year);
 }
 
 init();
