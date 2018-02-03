abceb3bf437b22bf033e0c3f87c32915d2529d4a
Sean Kelly <code@katron.org>
2018-02-03 08:10:43 -0500

    Move displaying the teams into a Season method

--- winsim.ts
+++ winsim.ts
@@ -27,6 +27,34 @@ class Season {
         }
         return _teams;
     }
+
+    displayTeams() {
+        let teams = document.createDocumentFragment();
+        for (let team of this.teams) {
+            let row = document.createElement("tr");
+            let name = document.createElement("td");
+            name.innerText = team.name;
+            let win = document.createElement("td");
+            win.innerText = team.win_percentage.toPrecision(3);
+            let pythagenpat = document.createElement("td");
+            pythagenpat.innerText = team.pythagenpat_percentage.toPrecision(3);
+            let baseruns = document.createElement("td");
+            baseruns.innerText = team.baseruns_percentage.toPrecision(3);
+
+            row.appendChild(name);
+            row.appendChild(win)
+            row.appendChild(pythagenpat)
+            row.appendChild(baseruns)
+            teams.appendChild(row);
+        }
+
+        let teams_table = document.getElementById("teams") as HTMLTableElement;
+        let teams_body = teams_table.tBodies[0];
+        while (teams_body.firstChild) {
+            teams_body.removeChild(teams_body.firstChild);
+        }
+        teams_body.appendChild(teams);
+    }
 }
 
 class Schedule {
@@ -60,6 +88,8 @@ class Team {
     }
 }
 
+let season = null;
+
 function init() {
     jQuery.getJSON('mlb.json')
         .done(function(data) {
@@ -83,31 +113,8 @@ function load_season(year) {
     let season_file = 'mlb-' + year + '.json';
     jQuery.getJSON(season_file)
         .done(function(data) {
-            let teams = document.createDocumentFragment();
-            for (let team of data.teams) {
-                let row = document.createElement("tr");
-                let name = document.createElement("td");
-                name.innerText = team.name;
-                let win = document.createElement("td");
-                win.innerText = team.win_percentage.toPrecision(3);
-                let pythagenpat = document.createElement("td");
-                pythagenpat.innerText = team.pythagenpat_percentage.toPrecision(3);
-                let baseruns = document.createElement("td");
-                baseruns.innerText = team.baseruns_percentage.toPrecision(3);
-
-                row.appendChild(name);
-                row.appendChild(win)
-                row.appendChild(pythagenpat)
-                row.appendChild(baseruns)
-                teams.appendChild(row);
-            }
-
-            let teams_table = document.getElementById("teams") as HTMLTableElement;
-            let teams_body = teams_table.tBodies[0];
-            while (teams_body.firstChild) {
-                teams_body.removeChild(teams_body.firstChild);
-            }
-            teams_body.appendChild(teams);
+            season = new Season(data.teams, data.schedule);
+            season.displayTeams();
         });
 }
 
