400ac5edc0b5132c4d7521133bf4a195b7889e78
Travis CI User <travis@example.org>
2018-02-03 20:35:09 +0000

    Deploy seankelly/winsim@ecfc3e130292

--- winsim.js
+++ winsim.js
@@ -1,98 +1,113 @@
-var Game = /** @class */ (function () {
-    function Game(game_data) {
+var GameResult;
+(function (GameResult) {
+    GameResult[GameResult["Unplayed"] = 0] = "Unplayed";
+    GameResult[GameResult["AwayWin"] = 1] = "AwayWin";
+    GameResult[GameResult["HomeWin"] = 2] = "HomeWin";
+})(GameResult || (GameResult = {}));
+class Game {
+    constructor(game_data) {
         this.date = game_data[0];
         this.away_team = game_data[1];
         this.home_team = game_data[2];
+        this.reset();
     }
-    return Game;
-}());
-var Season = /** @class */ (function () {
-    function Season(teams, schedule) {
-        this.teams = this.createTeams(teams);
+    reset() {
+        this.sim_result = GameResult.Unplayed;
+    }
+}
+class Season {
+    constructor(teams, schedule) {
+        this.createTeams(teams);
         this.schedule = new Schedule(schedule);
+        this.reset();
     }
-    Season.prototype.createTeams = function (teams) {
-        var _teams = [];
-        for (var _i = 0, teams_1 = teams; _i < teams_1.length; _i++) {
-            var team_data = teams_1[_i];
-            var team = new Team(team_data);
-            _teams.push(team);
+    createTeams(teams) {
+        for (let team_data of teams) {
+            let team = new Team(team_data);
+            this.teams[team.name] = team;
         }
-        return _teams;
-    };
-    Season.prototype.displayTeams = function () {
-        var teams = document.createDocumentFragment();
-        for (var _i = 0, _a = this.teams; _i < _a.length; _i++) {
-            var team = _a[_i];
-            var row = document.createElement("tr");
-            var name_1 = document.createElement("td");
-            name_1.innerText = team.name;
-            var win = document.createElement("td");
+    }
+    displayTeams() {
+        let teams = document.createDocumentFragment();
+        for (let team of this.teams.values()) {
+            let row = document.createElement("tr");
+            let name = document.createElement("td");
+            name.innerText = team.name;
+            let win = document.createElement("td");
             win.innerText = team.win_percentage.toPrecision(3);
-            var pythagenpat = document.createElement("td");
+            let pythagenpat = document.createElement("td");
             pythagenpat.innerText = team.pythagenpat_percentage.toPrecision(3);
-            var baseruns = document.createElement("td");
+            let baseruns = document.createElement("td");
             baseruns.innerText = team.baseruns_percentage.toPrecision(3);
-            row.appendChild(name_1);
+            row.appendChild(name);
             row.appendChild(win);
             row.appendChild(pythagenpat);
             row.appendChild(baseruns);
             teams.appendChild(row);
         }
-        var teams_table = document.getElementById("teams");
-        var teams_body = teams_table.tBodies[0];
+        let teams_table = document.getElementById("teams");
+        let teams_body = teams_table.tBodies[0];
         while (teams_body.firstChild) {
             teams_body.removeChild(teams_body.firstChild);
         }
         teams_body.appendChild(teams);
-    };
-    Season.prototype.displaySchedule = function () {
-        var schedule_dom = document.createDocumentFragment();
-        for (var _i = 0, _a = this.schedule.game; _i < _a.length; _i++) {
-            var game = _a[_i];
-            var row = document.createElement("tr");
-            var date = document.createElement("td");
+    }
+    displaySchedule() {
+        let schedule_dom = document.createDocumentFragment();
+        for (let game of this.schedule.game) {
+            let row = document.createElement("tr");
+            let date = document.createElement("td");
             date.innerText = game.date;
-            var home = document.createElement("td");
+            let home = document.createElement("td");
             home.innerText = game.home_team;
-            var away = document.createElement("td");
+            let away = document.createElement("td");
             away.innerText = game.away_team;
             row.appendChild(date);
             row.appendChild(home);
             row.appendChild(away);
             schedule_dom.appendChild(row);
         }
-        var schedule_table = document.getElementById("schedule");
-        var schedule_body = schedule_table.tBodies[0];
+        let schedule_table = document.getElementById("schedule");
+        let schedule_body = schedule_table.tBodies[0];
         while (schedule_body.firstChild) {
             schedule_body.removeChild(schedule_body.firstChild);
         }
         schedule_body.appendChild(schedule_dom);
-    };
-    return Season;
-}());
-var Schedule = /** @class */ (function () {
-    function Schedule(schedule_data) {
+    }
+    findTeam(team_name) {
+        return this.teams.get(team_name);
+    }
+    reset() {
+        this.schedule.reset();
+    }
+}
+class Schedule {
+    constructor(schedule_data) {
         this.game = [];
-        for (var _i = 0, schedule_data_1 = schedule_data; _i < schedule_data_1.length; _i++) {
-            var game_data = schedule_data_1[_i];
+        for (let game_data of schedule_data) {
             this.game.push(new Game(game_data));
         }
     }
-    return Schedule;
-}());
-var Simulation = /** @class */ (function () {
-    function Simulation() {
-        var iterations_input = document.getElementById('number-simulations');
+    reset() {
+        for (let game of this.game) {
+            game.reset();
+        }
+    }
+}
+class Simulation {
+    constructor() {
+        let iterations_input = document.getElementById('number-simulations');
         this.iterations = parseFloat(iterations_input.value);
     }
-    Simulation.prototype.run = function () {
+    run() {
         console.log("Running " + this.iterations + " simulations.");
-    };
-    return Simulation;
-}());
-var Team = /** @class */ (function () {
-    function Team(team_data) {
+        for (let iteration = 0; iteration < this.iterations; iteration++) {
+            season.reset();
+        }
+    }
+}
+class Team {
+    constructor(team_data) {
         this.name = team_data.name;
         this.league = team_data.league;
         this.win_percentage = team_data.win_percentage;
@@ -101,31 +116,30 @@ var Team = /** @class */ (function () {
         this.wins = team_data.wins;
         this.losses = team_data.losses;
     }
-    return Team;
-}());
-var season = null;
+}
+let season = null;
+let simulation = null;
 function init() {
-    var start_button = document.getElementById("start-simulations");
+    let start_button = document.getElementById("start-simulations");
     start_button.onclick = start_simulations;
     jQuery.getJSON('mlb.json')
         .done(function (data) {
-        var season_select = document.getElementById('season-select');
-        var seasons = document.createDocumentFragment();
-        for (var _i = 0, _a = data.years; _i < _a.length; _i++) {
-            var year = _a[_i];
-            var option = document.createElement("option");
+        let season_select = document.getElementById('season-select');
+        let seasons = document.createDocumentFragment();
+        for (let year of data.years) {
+            let option = document.createElement("option");
             option.value = year;
             option.innerText = year;
             seasons.appendChild(option);
         }
         season_select.appendChild(seasons);
-        var select_text = season_select.children[0];
+        let select_text = season_select.children[0];
         select_text.disabled = true;
         season_select.onchange = pick_season;
     });
 }
 function load_season(year) {
-    var season_file = 'mlb-' + year + '.json';
+    let season_file = 'mlb-' + year + '.json';
     jQuery.getJSON(season_file)
         .done(function (data) {
         season = new Season(data.teams, data.schedule);
@@ -134,7 +148,7 @@ function load_season(year) {
     });
 }
 function pick_season(ev) {
-    var year = ev.target.value;
+    let year = ev.target.value;
     if (!year) {
         return;
     }
@@ -145,7 +159,7 @@ function start_simulations(ev) {
         console.log("No season loaded.");
         return;
     }
-    var simulation = new Simulation();
+    simulation = new Simulation();
     simulation.run();
 }
 init();
